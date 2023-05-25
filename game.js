2511feb3f5aab33fe857806614e861bc1feed788
Miles Berman <mhberman@ucsc.edu>
2023-05-24 17:48:16 -0700

    First final demo

---
+++ game.js
@@ -0,0 +1,252 @@
+class GameLevel extends Phaser.Scene {
+    init(data) {
+        this.location = data.location || {r: 3, c: 1};
+        this.playerLocation = data.playerLocation || {x: 912, y: 492};
+        this.playerChased = data.playerChased || false;
+        this.monsterLocation = data.monsterLocation || {r: 0, c: 0};
+        this.chaseTime = data.chaseTime || 0;
+        this.caught = data.caught || false;
+        this.everChased = data.everChased || false;
+    }
+
+    constructor(key, name) {
+        super(key);
+        this.name = name;
+    }
+
+    create() {
+        this.transitionDuration = 500;
+
+        this.w = this.game.config.width;
+        this.h = this.game.config.height;
+        this.s = this.game.config.width * 0.01;
+
+        this.speed = 500;
+
+        this.cameras.main.setBackgroundColor('#444');
+        this.cameras.main.fadeIn(this.transitionDuration, 0, 0, 0);
+        this.map = this.cache.json.get('map');
+
+        this.initializePlayer();
+
+        // fullscreen
+        this.input.keyboard.on('keydown-' + 'F', () => { 
+            if (this.scale.isFullscreen) {
+                this.scale.stopFullscreen();
+            } else {
+                this.scale.startFullscreen();
+            }
+        });
+
+        // textBox
+        this.shown = false;
+        this.input.keyboard.on('keydown-' + 'T', () => { 
+            if (!this.shown) {
+                this.showTextBox("Hi! How are you?", 100, 0);
+            } else {
+                this.hideTextBox();
+            }
+            this.shown = !this.shown;
+        });
+
+        this.onEnter();
+    }
+
+    showTextBox(t, size, i) { // text, font size, which spritesheet icon to use
+        this.bubble = this.add.image(this.w * 0.44, this.h * 0.85, 'speechBubble').setOrigin(0.5, 0.5).setAlpha(1).setScale(1.16, 1);
+        this.text = this.add.text(this.w * 0.35, this.h * 0.85, t, { color: '#000000', fontSize: size })
+            .setOrigin(0.5, 0.5)
+            .setStroke(0x000000, 5);
+        this.i = this.add.image(this.w * 0.9, this.h * 0.81, 'icon', i).setAlpha(1).setScale(3); // i == 0 is neutral, i == 1 is irritated
+    }
+
+    hideTextBox() {
+        this.bubble.setAlpha(0);
+        this.text.setAlpha(0);
+        this.i.setAlpha(0);
+    }
+
+    initializePlayer() {
+        this.player = this.physics.add.image(this.playerLocation.x, this.playerLocation.y, 'player')
+            .setOrigin(0, 0);
+    }
+
+    gotoScene(key) {
+        //this.blackedOut.setAlpha(0);
+        this.cameras.main.fade(this.transitionDuration, 0, 0, 0);
+        this.time.delayedCall(this.transitionDuration, () => {
+            this.scene.start(key, {
+                location: this.location,
+                playerLocation: this.playerLocation,
+                playerChased: this.playerChased,
+                chaseTime: this.chaseTime,
+                monsterLocation: this.monsterLocation,
+                caught: this.caught,
+                everChased: this.everChased
+            });
+        });
+    }
+
+    chase(c) {
+        if (!this.playerChased && ((this.location.r == 0 && !this.everChased) || c == 1)) { // Player starts getting chased when they reach the top of the map
+            this.everChased = true;
+            this.playerChased = !this.playerChased;
+            let run = this.add.text(this.w * 0.52, this.h * 0.3, "Run!", { color: '#710C04', fontSize: 150 })
+                .setOrigin(0.5, 0.5)
+                .setStroke(0x000000, 5);
+            this.tweens.add({
+                targets: run,
+                alpha: 0,
+                ease: "Linear",
+                duration: 4000, 
+            });
+            if (c == 1) { // if key was pressed to start chase
+                if (this.location.r == 0 || this.location.r == 1) {
+                    this.monsterLocation.r = 2; // Monster at least 2 rooms below player
+                }
+                else {
+                    this.monsterLocation.r = 0; // Monster at least 2 rooms below player
+                }
+            }
+            else {
+                this.monsterLocation.r = 2; // Monster at least 2 rooms below player
+            }
+            this.monsterLocation.c = Phaser.Math.Between(0, 2); // Monster in a random column 
+        }
+        if (this.playerChased && this.location.r == 3 && this.location.c == 1) { // safe room stops the player from being chased
+            this.playerChased = !this.playerChased;
+            this.everChased = false;
+        }
+    }
+
+    updateMonsterLocation() {
+        let r = Phaser.Math.Between(0, 2);
+        if (r === 0) { // 1 in 3 to move the monster 2 rooms closer
+            this.updateMonsterRow();
+            this.updateMonsterColumn();
+        }
+        else if ((r == 1 && this.monsterLocation.r != this.location.r) || (r == 2 && this.monsterLocation.c == this.location.c)) { // change monsterLocation.r
+            this.updateMonsterRow();
+        }
+        else { // change monsterLocation.c
+            this.updateMonsterColumn();
+        }
+        this.checkMonsterWarning();
+    }
+
+    checkMonsterWarning() {
+        // if you escape when cross
+        if(this.playerChased && this.caught && (this.monsterLocation.r != this.location.r || this.monsterLocation.c != this.location.c)) { // if the player was caught but got away
+            this.playerChased = !this.playerChased;
+            this.caught = !this.caught;
+            //console.log("No longer being chased");
+        }
+        
+        if (this.playerChased) {
+            if (this.monsterLocation.r == this.location.r && this.monsterLocation.c == this.location.c) {
+                this.blackedOut = this.add.rectangle(0, 0, this.w, this.h)
+                    .setOrigin(0,0)
+                    .setFillStyle(0x000000);
+                this.caught = !this.caught;
+                let warning = this.add.text(this.w * 0.5, this.h * 0.3, "Get out now!", { color: '#ffffff', fontSize: 150, fontStyle: 'bold' })
+                    .setOrigin(0.5, 0.5)
+                    .setAlign('center')
+                    .setStroke(0x000000, 5);
+                this.tweens.add({
+                    targets: warning,
+                    alpha: 0,
+                    ease: "Linear",
+                    duration: 3000, 
+                });
+            }
+            let row_diff = this.monsterLocation.r - this.location.r;
+            let col_diff = this.monsterLocation.c - this.location.c;
+            if(row_diff == -1 && col_diff >= -1 && col_diff <= 1) { // North Wall should start flashing red
+                this.NW.setFillStyle(0x710C04);
+                this.NWTween = this.tweens.add({
+                    targets: this.NW,
+                    alpha: { from: 1, to: 0.5 },
+                    ease: "Sine.InOut",
+                    repeat: -1,
+                    yoyo: true,
+                    duration: 500, 
+                });
+            }
+            else if (this.NWTween){ // North Wall should be normal
+                this.NWTween.stop();
+                this.NW.setFillStyle(0x323232);
+            }
+
+            if(row_diff == 1 && col_diff >= -1 && col_diff <= 1) { // South Wall should start flashing red
+                this.SW.setFillStyle(0x710C04);
+                this.SWTween = this.tweens.add({
+                    targets: this.SW,
+                    alpha: { from: 1, to: 0.5 },
+                    ease: "Sine.InOut",
+                    repeat: -1,
+                    yoyo: true,
+                    duration: 500, 
+                });
+            }
+            else if (this.SWTween){ // South Wall should be normal
+                this.SWTween.stop();
+                this.SW.setFillStyle(0x323232);
+            }
+
+            if(col_diff == 1 && row_diff >= -1 && row_diff <= 1) { // East Wall should start flashing red
+                this.EW.setFillStyle(0x710C04);
+                this.EWTween = this.tweens.add({
+                    targets: this.EW,
+                    alpha: { from: 1, to: 0.5 },
+                    ease: "Sine.InOut",
+                    repeat: -1,
+                    yoyo: true,
+                    duration: 500, 
+                });
+            }
+            else if (this.EWTween) { // East Wall should be normal
+                this.EWTween.stop();
+                this.EW.setFillStyle(0x323232);
+            }
+
+            if(col_diff == -1 && row_diff >= -1 && row_diff <= 1) { // West Wall should start flashing red
+                this.WW.setFillStyle(0x710C04);
+                this.WWTween = this.tweens.add({
+                    targets: this.WW,
+                    alpha: { from: 1, to: 0.5 },
+                    ease: "Sine.InOut",
+                    repeat: -1,
+                    yoyo: true,
+                    duration: 500, 
+                });
+            }
+            else if (this.WWTween) { // West Wall should be normal
+                this.WWTween.stop();
+                this.WW.setFillStyle(0x323232);
+            }
+        }
+
+    }
+
+    updateMonsterRow() { // moves the monster 1 row closer to the player
+        if (this.monsterLocation.r < this.location.r) {
+            this.monsterLocation.r++;
+        }
+        else if (this.monsterLocation.r > this.location.r) {
+            this.monsterLocation.r--;
+        }
+    }
+
+    updateMonsterColumn() { // moves the monster 1 column closer to the player
+        if (this.monsterLocation.c < this.location.c) {
+            this.monsterLocation.c++;
+        }
+        else if (this.monsterLocation.c > this.location.c) {
+            this.monsterLocation.c--;
+        }
+    }
+
+    onEnter() {
+        console.warn('This GameScene did not implement onEnter():', this.constructor.name);
+    }
+}
---
+++ room.js
@@ -0,0 +1,176 @@
+class Start extends GameLevel {
+    constructor() {
+        super('start', 'Level');
+    }
+
+    preload() {
+        this.load.image('player', 'assets/player.png');
+        this.load.image('speechBubble', 'assets/speechBubble.png');
+        this.load.spritesheet('icon', 'assets/spritesheet.png', { frameWidth: 100, frameHeight: 100 });
+        this.load.json('map', 'map.json');
+    }
+
+    onEnter() {
+        //let room = this.map.Levels[this.location.r][this.location.c];
+        //console.log(room.Doors);
+        //console.log(this.location);
+        this.chase(0);
+
+        this.initializeDoors();
+        this.setCollision();
+
+        this.checkMonsterWarning();
+
+        // inputs
+        const {LEFT, RIGHT, UP, DOWN, W, A, S, D, C} = Phaser.Input.Keyboard.KeyCodes;
+        this.keys = this.input.keyboard.addKeys({
+            left: LEFT,
+            right: RIGHT,
+            up: UP,
+            down: DOWN,
+            w: W,
+            a: A,
+            s: S,
+            d: D,
+            c: C
+        });
+    }
+
+    update(time, delta) {
+
+        if (this.playerChased) {
+            this.chaseTime += delta;
+            if (this.chaseTime > 5000) { // every 5 seconds of chasing, the monster will get closer
+                this.chaseTime -= 5000;
+                this.updateMonsterLocation();
+            }
+        }
+        
+        const {keys} = this; // this.keys
+        
+        this.player.setVelocity(0);
+        // movement
+        if(keys.left.isDown || keys.a.isDown) {
+            this.player.setVelocityX(-this.speed);
+        }
+        if(keys.right.isDown || keys.d.isDown) {
+            this.player.setVelocityX(this.speed);
+        }
+        if(keys.up.isDown || keys.w.isDown) {
+            this.player.setVelocityY(-this.speed);
+        } 
+        if(keys.down.isDown || keys.s.isDown) {
+            this.player.setVelocityY(this.speed);
+        } 
+
+        // lets the player get chased again
+        if(keys.c.isDown) {
+            this.chase(1);
+        } 
+    }
+
+    setCollision() {
+        this.sceneChanged = false; // makes sure scene only changes once
+        
+        this.physics.add.collider(this.NWall, this.player);
+        this.physics.add.collider(this.WWall, this.player);
+        this.physics.add.collider(this.EWall, this.player);
+        this.physics.add.collider(this.SWall, this.player);
+
+        if(this.NDoor) {
+            this.physics.add.collider(this.NDoor, this.player, () => {this.gotoDoor(true, -1, 912, this.h * 0.8);}, null, this);
+        }
+        if(this.SDoor) {
+            this.physics.add.collider(this.SDoor, this.player, () => {this.gotoDoor(true, 1, 912, this.h * 0.12);}, null, this);
+        }
+        if(this.EDoor) {
+            this.physics.add.collider(this.EDoor, this.player, () => {this.gotoDoor(false, 1, this.w * 0.06, 492);}, null, this);
+        }
+        if(this.WDoor) {
+            this.physics.add.collider(this.WDoor, this.player, () => {this.gotoDoor(false, -1, this.w * 0.9, 492);}, null, this);
+        }
+    }
+
+    gotoDoor(r, l, x, y) {
+        if (!this.sceneChanged) {
+            this.sceneChanged = true;
+            if (r) { // move up or down in the grid when going to a new scene
+                this.location.r += l;
+            }
+            else { // move left or right in the grid when going to a new scene
+                this.location.c += l;
+            }
+            this.playerLocation.x = x;
+            this.playerLocation.y = y;
+            this.gotoScene('start');
+        }
+    }
+
+    initializeDoors() {
+        let room = this.map.Levels[this.location.r][this.location.c];
+        this.createWalls();
+        this.NDoor;
+        this.WDoor;
+        this.EDoor;
+        this.SDoor;
+        if (room.Doors.N === 1) {
+            this.ND = this.createDoor(this.w * 0.45, 0, this.w * 0.1, this.h * 0.075, true, -1);
+            this.NDoor = this.physics.add.existing(this.ND, true);
+        }
+        if (room.Doors.S === 1) {
+            this.SD = this.createDoor(this.w * 0.45, this.h * 0.925, this.w * 0.1, this.h * 0.075, true, 1);
+            this.SDoor = this.physics.add.existing(this.SD, true);
+        }
+        if (room.Doors.E === 1) {
+            this.ED = this.createDoor(this.w * 0.9625, this.h * 0.425, this.w * 0.0375, this.h * 0.15, false, 1);
+            this.EDoor = this.physics.add.existing(this.ED, true);
+        }
+        if (room.Doors.W === 1) {
+            this.WD = this.createDoor(0, this.h * 0.425, this.w * 0.0375, this.h * 0.15, false, -1);
+            this.WDoor = this.physics.add.existing(this.WD, true);
+        }
+    }
+
+    createWalls() {
+        // Creates North West East and South walls
+        this.NW = this.add.rectangle(0, 0, this.w, this.h * 0.05)
+            .setOrigin(0,0)
+            .setFillStyle(0x323232);
+        this.WW = this.add.rectangle(0, 0, this.w * 0.025, this.h)
+            .setOrigin(0,0)
+            .setFillStyle(0x323232);
+        this.EW = this.add.rectangle(this.w * 0.975, 0, this.w * 0.025, this.h)
+            .setOrigin(0,0)
+            .setFillStyle(0x323232);
+        this.SW = this.add.rectangle(0, this.h * 0.95, this.w, this.h * 0.05)
+            .setOrigin(0,0)
+            .setFillStyle(0x323232);
+        this.NWall = this.physics.add.existing(this.NW, true);
+        this.WWall = this.physics.add.existing(this.WW, true);
+        this.EWall = this.physics.add.existing(this.EW, true);
+        this.SWall = this.physics.add.existing(this.SW, true);
+    }
+
+    createDoor(x1, y1, x2, y2, row_change, location_change) {
+        let r = this.add.rectangle(x1, y1, x2, y2)
+            .setOrigin(0, 0)
+            .setFillStyle(0x42280E);
+        return r;
+    }
+}
+
+const game = new Phaser.Game({
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 1920,
+        height: 1080
+    },
+    physics: {
+        default: 'arcade',
+        //arcade: { debug: true }
+    },
+    backgroundColor: 0x212121,
+    scene: [Start],
+    title: "Chase",
+});
